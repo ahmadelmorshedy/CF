37cf30f7fb1adc2caf96fb6e7cd96d8c47b00794
ahmadelmorshedy <ah.morshedy@gmail.com>
2017-11-13 02:40:55 +0200

    Changing/Adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'weather/getWeather'
+  # Weather routes
+  root :to => "weather#weather"
+  get 'weather/get_weather' => 'weather#get_weather', as: :get_weather
+
+  # Cities routes
+  post 'country_cities_select' => "weather#country_cities_select"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
